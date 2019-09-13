076bc3e8b8f540c28411d9b4d696a875817cd575
onluiz <luiz.alberto1723@gmail.com>
2019-09-13 15:23:02 -0300

    Add basic asserts

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <section class="section">
-        <div class="container">
-          <h1 class="title">
-            Hello World
+      <section className="section">
+        <div className="container">
+          <h1 className="title">
+            Movies
           </h1>
-          <p class="subtitle">
-            My first website with <strong>Bulma</strong>!
+          <p className="subtitle">
+            An incredible website to search for <strong>movies</strong>!
           </p>
         </div>
       </section>
