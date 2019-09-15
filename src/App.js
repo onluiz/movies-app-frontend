3117d39c5b4e1f7998ef06cb40f6dde19e72ee0b
onluiz <luiz.alberto1723@gmail.com>
2019-09-15 19:34:52 -0300

    Update App.js with first draft of the app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import MovieList from './components/MovieList';
+import MovieSearch from './components/MovieSearch';
+import moviesApi from './libs/movies-api';
 
-function App() {
-  return (
-    <div>
-      <section className="section">
-        <div className="container">
-          <h1 className="title">
-            Movies
-          </h1>
-          <p className="subtitle">
-            An incredible website to search for <strong>movies</strong>!
-          </p>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      movies: [],
+      page: 1,
+    }
+  }
+
+  async componentDidMount() {
+    this.loadUpcomingMovies()
+  }
+
+  loadMore = async () => {
+    this.incrementPage()
+    const response = await moviesApi.getUpcomingMovies(this.state.page)
+    const { results } = response.data
+    this.setState({
+      ...this.state,
+      movies: this.state.movies.concat(results),
+    })
+  }
+
+  loadUpcomingMovies = async () => {
+    try {
+      const response = await moviesApi.getUpcomingMovies(this.state.page)
+      const { results, page } = response.data
+      this.setState({ ...this.state, movies: results, page })
+    } catch(error) {
+      console.error(error)
+    }
+  }
+
+  incrementPage = () => {
+    this.setState({ ...this.state, page: this.state.page + 1 })
+  }
+
+  onSearchDone = ({ results, page }) => {
+    this.setState({...this.state, movies: results, page })
+  }
+
+  onSearchClean = () => {
+    this.loadUpcomingMovies()
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div>
+          <br />
+          <br />
+          <br />
+          <MovieSearch onSearchDone={this.onSearchDone} onSearchClean={this.onSearchClean}/>
+          <MovieList />
         </div>
-      </section>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
