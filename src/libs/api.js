6b6de83c4943e654355c2e1aee1c79dfc6b83d8d
onluiz <luiz.alberto1723@gmail.com>
2019-09-15 20:19:38 -0300

    Review api calls organization

--- src/libs/api.js
+++
@@ -1,3 +0,0 @@
-const BASE_URL = `${process.env.REACT_APP_API}/${process.env.REACT_APP_STAGE}`
-
-export default BASE_URL
--- src/libs/movies-api.js
+++ src/libs/movies-api.js
@@ -1,15 +1,17 @@
 import axios from 'axios'
-import BASE_URL from './api'
+
+const BASE_URL = `${process.env.REACT_APP_API}/${process.env.REACT_APP_STAGE}`
+const moviesApiInstance = axios.create({ baseURL: BASE_URL });
 
 const moviesApi = {
   getUpcomingMovies(page = 1) {
-    return axios.get(`${BASE_URL}/movies/upcoming?page=${page}`)
+    return moviesApiInstance.get(`/movies/upcoming?page=${page}`)
   },
   getById(id) {
-    return axios.get(`${BASE_URL}/movies/${id}`)
+    return moviesApiInstance.get(`/movies/${id}`)
   },
   searchByName(movieName = '') {
-    return axios.get(`${BASE_URL}/movies?page=1&query=${movieName}`)
+    return moviesApiInstance.get(`/movies?page=1&query=${movieName}`)
   },
   getImage(path = '') {
     return `${BASE_URL}/movies/image?imagePath=${path}`
